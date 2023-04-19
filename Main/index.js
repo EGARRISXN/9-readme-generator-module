59bd97bacfc62c311cae5d07e7850f0054da194d
Ethan Garrison <egarrisxn@gmail.com>
2023-04-19 01:18:58 -0400

    worked on the generatemarkdown and index js

--- Main/index.js
+++ Main/index.js
@@ -1,71 +1,108 @@
 //Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('generateMarkdown');
+const generateMarkdown = require('../Main/utilities/generatemarkdown');
 
 
 //Array of questions for user input
 const questions = [    
     {
     type: 'input',
-    message: 'What is the title?',
+    message: 'What is the title of the program?',
     name: 'title',
-    },
+    validate: titleInput => {
+        if (titleInput) {
+            return true;
+        } else {
+            console.log('Please enter the title of the program.');
+            return false;
+        }
+    }},
     {
     type: 'input',
-    message: 'How do you install the application?',
-    name: 'install',
-    },
+    message: 'What is a brief description of the program?',
+    name: 'description',
+    validate: descriptionInput => {
+        if (descriptionInput) {
+            return true;
+        } else {
+            console.log('Please enter a brief description of the program.');
+            return false;
+        }
+    }},
     {
     type: 'input',
-    message: 'What are the instructions?',
-    name: 'instructions',
-    },
+    message: 'How do you install this program?',
+    name: 'installation',
+    validate: installationInput => {
+        if (installationInput) {
+            return true;
+        } else {
+            console.log('Please describe how to install this program.');
+            return false;
+        }
+    }},
     {
     type: 'input',
-    message: 'Do you have any credits?',
-    name: 'credits',
-    },
-    {
-    type: 'input',
-    message: 'How do you use the application?',
+    message: 'How do you run the program?',
     name: 'usage',
-    },
+    validate: usageInput => {
+        if (usageInput) {
+            return true;
+        } else {
+            console.log('Please describe how to run this program.');
+            return false;
+        }
+    }},
     {
-    type: 'input',
-    message: 'Did you use any licenses?',
+    type: 'list',
+    message: 'Did you use a license for this program?',
     name: 'license',
-    },
+    choices: ['MIT', 'APACHE 2.0', 'GPL v3.0', 'NONE'],
+    validate: licenseInput => {
+        if (licenseInput) {
+            return true;
+        } else {
+            console.log('Please enter a license for this program.');
+            return false;
+        }
+    }},
     {
     type: 'input',
-    message: 'What is your Github username?',
-    name: 'github',
-    },
-    {
-    type: 'input',
-    message: 'What is your email address?',
-    name: 'email',
-    }
-  ];
+    message: 'Did you have any contributors for this program?',
+    name: 'contributors',
+    validate: contributorsInput => {
+        if (contributorsInput) {
+            return true;
+        } else {
+            console.log('Please list any contributors for this program.');
+            return false;
+        }
+    }},
+];
   
-  //Function to write README file
-  function writeToFile(fileName, data) {
-    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.nd`,data,(err)=>{
+//Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.md`,data,(err)=> {
         if(err){
             console.log(err)
         }
-        console.log('README complete!');
+        console.log('README.md complete!');
     })
-  }
+}
   
-  //Function that initializes the app
-  function init() {
+//Function that initializes the app
+function init() {
+    inquirer.prompt(questions).then(function(response) {
+        console.log(response);
+
+    let data = generateMarkdown(response);
+        writeToFile(data)
+    });
+};
   
-      // inquirer goes in here -> questions passed as argument --- .then call back function (writeToFile(){}) will will use generateMarkdown as data source --- pass data from inquirer into this!! how is spread operator to be used in this?
-  }
-  
-  //Function call to initialize app
-  init();
+//Function call to initialize app
+init();
 
 
 
--- Main/utilities/generatemarkdown.js
+++ Main/utilities/generatemarkdown.js
@@ -1,45 +1,79 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+const inquirer = require('inquirer');
+const fs = require('fs');
+const index = require('../index');
+
+
+//Function that returns a license badge based on which license is passed in
 function renderLicenseBadge(license) {
-  // this will use data passed in from generateMarkdown function. have a logic gate to make sure a license was chosen.
+  let badge = '';
+  if(license === 'MIT') {
+    badge = ''
+  } else if (license === 'APACHE 2.O') {
+    badge = ''
+  } else if (license === 'GPL v3.0') {
+    badge = ''
+  } else {
+    badge = ''
+  }
   return `https://img.shields.io/badge/license-${license}-blue`
 }
 
-// TODO: Create a function that returns the license link - this is internally in the .md file...a link to the license section
-// If there is no license, return an empty string
+//Function that returns the license link - this is internally in the .md file...a link to the license section
 function renderLicenseLink(license) {
-  // this will use data passed in from generateMarkdown function. have a logic gate to make sure a license was chosen.
+  let licenseLink = '';
+  if(license === 'MIT') {
+    licenseLink = ''
+  } else if (license === 'APACHE 2.O') {
+    licenseLink = ''
+  } else if (license === 'GPL v3.0') {
+    licenseLink = ''
+  } else {
+    licenseLink = ''
+  }
+  return `https://img.shields.io/badge/license-${license}-blue`
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+//Function that returns the license section of README
 function renderLicenseSection(license) {
-  // this will use data passed in from generateMarkdown function.  have a logic gate to make sure a license was chosen.
+  let licenseSection = ''
+  if(license === 'NONE') {
+    licenseSection = ''
+  } else {
+    licenseSection = 
+    ` License: ${license} `
+  }
+  return licenseSection;
 }
 
-// TODO: Create a function to generate markdown for README
+//Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-    
+  return `
+  # ${data.title}
+
+  ## A description about your program.
+  ## ${response.description}
+  
+  ### Installation
+  ### ${response.installation}
+
+  ### Usage
+  ### ${response.usage}
+
+  ## License
+  ## ${response.license}
+
+  ## Contributors
+  ## ${response.contributors} 
 `;
-  // you will need to use the license functions in here ^, passing data to the functions you call -- licenseFunction(data.license)
 }
 
 module.exports = generateMarkdown;
 
 
-
-
 //for this you'll return a template literal. this will be the markdown language
-
 //write general readme first then paste into template literal to see if it works
-
 //license up top will be used in the template literal generatemarkdown
-
 //since (data) is the parameter and the data getting passed is the questions... the data.license is what youre going to pass into whatever license.function is and thats going to be the argument for the (license) parameter 
-
 //to render the license badge you'll use the data that's been passed around rather what kind of license or if there is one at all YOU WILL HAVE A LOGIC GATE -- use shields.io for this *also refer to screenshot* fill in message on shields site with data you get from inquirer prompt. USE A TEMPLATE LITERAL
-
 //data passing function example. kitchen. chef to sous chef. produce. etc.
-
 //init is head chef tells sous chef what to do. generatemarkdown and other functions is the sous chef and processes the produce. data or questions array is the produce.
